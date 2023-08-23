10012750d6b2e956581587a2b15fa145d354dced
Rawan.H <rawanhawangledt@gmail.com>
2023-08-23 20:29:31 -0300

    Implementação Das Rotas de Get, Login & Sistemas de rotas privadas

--- semana04/backend/src/controllers/user.controller.js
+++ semana04/backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const { sign } = require('jsonwebtoken')
 const { User } = require('../models/User')
 
 class UserController {
@@ -19,6 +20,66 @@ class UserController {
         } catch (error) {
             return res.status(400).send({
                 message: "Erro na criação de usuário",
+                cause: error.message
+            })
+        }
+    }
+
+    async findAll(req, res) {
+        try {
+            const users = await User.findAll()
+
+            return res.status(200).send(users)
+        } catch (error) {
+            return res.status(400).send({
+                message: "Erro ao listar todos os usuários",
+                cause: error.message
+            })
+        }
+    }
+
+    async findOne(req, res) {
+        try {
+            const { userId } = req.params
+            const user = await User.findByPk(userId)
+
+            return res.status(200).send(`Usuário: ${user.email}`)
+        } catch (error) {
+            return res.status(400).send({
+                message: "Erro ao listar o usuário",
+                cause: error.message
+            })
+        }
+    }
+
+    async login(req, res) {
+        try {
+            const { email, password } = req.body
+
+            const user = await User.findOne({
+                where: { email }
+            })
+
+            if(!user){
+                return res.status(404).send({message: "Usuário não Encontrado!"})
+            }
+
+            if ( user.password === password ) {
+                const payload = {
+                    name: user.name,
+                    email: user.email
+                }
+
+                const token = sign(payload, process.env.JWT_SECRET)
+                return res.status(200).send({token})
+            }
+            else {
+                return res.status(400).send({message: "Senha inválida"})
+            }
+
+        } catch (error) {
+            return res.status(400).send({
+                message: "Erro ao realizar o login do usuário",
                 cause: error.message
             })
         }
--- semana04/backend/src/routes/users.routes.js
+++ semana04/backend/src/routes/users.routes.js
@@ -1,11 +1,16 @@
-const { create } = require('../controllers/user.controller')
+const { create, findAll, findOne, login } = require('../controllers/user.controller')
 const { Router } = require('express')
+const { auth } = require('../middlewares/auth.middleware')
 
 
 class UserRouter{
     routesFromUser() {
         const userRoutes = Router()
         userRoutes.post('/users', create)
+        userRoutes.get('/users', auth , findAll)
+        userRoutes.get('/users/:userId', auth, findOne)
+        userRoutes.post('/users/login', login)
+
         return userRoutes
     }
 }
